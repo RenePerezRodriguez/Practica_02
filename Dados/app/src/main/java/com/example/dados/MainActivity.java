93ca8aa1f79b9a8bdf36d80ab8238e6909832dcd
sergio lara <sergiolara88@hotmail.com>
2020-04-23 23:06:32 -0400

    Cambios

--- Dados/app/src/main/java/com/example/dados/MainActivity.java
+++ Dados/app/src/main/java/com/example/dados/MainActivity.java
@@ -2,13 +2,100 @@ package com.example.dados;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.ContentValues;
+import android.database.sqlite.SQLiteDatabase;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import java.util.Random;
 
 public class MainActivity extends AppCompatActivity {
 
+    private EditText entradaDado;
+    private Button lanzar;
+    private TextView estado,numeroAleatorio;
+
+
+    //private static final int num = 10;
+    //int ramdon = (int)((Math.random()* num)+1);
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+
+        entradaDado = findViewById();
+
+        lanzar = findViewById();
+
+
+        estado = findViewById();
+        numeroAleatorio = findViewById();
+
+
+    }
+
+
+    public int devolverNumero(View view)
+    {
+        int puntuacion=0;
+        int dado = Integer.parseInt(entradaDado.getText().toString());
+
+        if (dado >= 1 && dado <= 10){
+
+            //llama a la clase
+            SqlHelper admin = new SqlHelper(this,
+                    "administracion", null, 1);
+            SQLiteDatabase bd = admin.getWritableDatabase();
+            ContentValues valor = new ContentValues();
+
+            valor.put("punto" , puntuacion);
+
+
+
+            if (dado == generarNumeroAleatorio())
+            {
+                puntuacion++;
+                bd.insert("tablaAumento",null , valor);
+                bd.close();
+                estado.setText(String.format("Puntuacion %d", puntuacion));
+            }
+            else{
+
+                estado.setText(String.format("Puntuacion %d", puntuacion));
+            }
+
+        }
+
+        else {
+
+            Toast.makeText(getApplicationContext(),"Debe ingresar un numero valido del 1 al 10",Toast.LENGTH_LONG);
+        }
+
+
+
+
+        return  puntuacion;
+    }
+
+
+
+    public int generarNumeroAleatorio(){
+
+
+        Random aleatorio = new Random(System.currentTimeMillis());
+        // Producir nuevo int aleatorio entre 0 y 9
+        int intAletorio = (aleatorio.nextInt(10) + 1);
+
+
+        // Refrescar datos aleatorios
+        aleatorio.setSeed(System.currentTimeMillis());
+
+        return  intAletorio;
+
     }
 }
---
+++ Dados/app/src/main/java/com/example/dados/SqlHelper.java
@@ -0,0 +1,27 @@
+package com.example.dados;
+
+import android.content.Context;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteOpenHelper;
+
+import androidx.annotation.Nullable;
+
+public class SqlHelper extends SQLiteOpenHelper {
+
+    private static final String tablaAumento = "CREATE TABLE  puntos(punto int)";
+
+    public SqlHelper(@Nullable Context context, @Nullable String name, @Nullable SQLiteDatabase.CursorFactory factory, int version) {
+        super(context, name, factory, version);
+    }
+
+
+    @Override
+    public void onCreate(SQLiteDatabase db) {
+        db.execSQL(tablaAumento);
+    }
+
+    @Override
+    public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
+
+    }
+}
